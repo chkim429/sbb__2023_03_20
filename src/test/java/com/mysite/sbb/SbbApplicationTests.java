e5d3896b759a91351d18c54c68a4d3ec2ad605b5
chkim429 <ckdgjs000@naver.com>
2023-03-21 11:09:07 +0900

    각 테스트케이스의 독립서성 확보

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,5 +1,6 @@
 package com.mysite.sbb;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,15 +18,23 @@ class SbbApplicationTests {
     @Autowired
     private QuestionRepository questionRepository;
 
-    @Test
-    @DisplayName("데이터 저장")
-    void t001() {
+    @BeforeEach
+        // 아래 메서드는 각 테스트케이스가 실행되기 전에 실행된다.
+    void beforeEach() {
+        // 모든 데이터 삭제
+        questionRepository.deleteAll();
+
+        // 흔적삭제(다음번 INSERT 때 id가 1번으로 설정되도록)
+        questionRepository.clearAutoIncrement();
+
+        // 질문 1개 생성
         Question q1 = new Question();
         q1.setSubject("sbb가 무엇인가요?");
         q1.setContent("sbb에 대해서 알고 싶습니다.");
         q1.setCreateDate(LocalDateTime.now());
         questionRepository.save(q1);  // 첫번째 질문 저장
 
+        // 질문 1개 생성
         Question q2 = new Question();
         q2.setSubject("스프링부트 모델 질문입니다.");
         q2.setContent("id는 자동으로 생성되나요?");
@@ -33,6 +42,23 @@ class SbbApplicationTests {
         questionRepository.save(q2);  // 두번째 질문 저장
     }
 
+    @Test
+    @DisplayName("데이터 저장")
+    void t001() {
+        // 질문 1개 생성
+        Question q = new Question();
+        q.setSubject("세계에서 가장 부유한 국가가 어디인가요?");
+        q.setContent("알고 싶습니다.");
+        q.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q);
+
+        assertEquals("세계에서 가장 부유한 국가가 어디인가요?", questionRepository.findById(3).get().getSubject());
+    }
+
+    /*
+    SQL
+    SELECT * FROM question
+    */
     @Test
     @DisplayName("findAll")
     void t002() {
@@ -103,26 +129,45 @@ class SbbApplicationTests {
         assertEquals("sbb가 무엇인가요?", q.getSubject());
     }
 
+    /*
+    SQL
+    UPDATE
+        question
+    SET
+        content = ?,
+        create_date = ?,
+        subject = ?
+    WHERE
+        id = ?
+    */
     @Test
     @DisplayName("데이터 수정하기")
-    void t007(){
-        Optional<Question> oq = this.questionRepository.findById(1);
+    void t007() {
+        Optional<Question> oq = questionRepository.findById(1);
         assertTrue(oq.isPresent());
         Question q = oq.get();
         q.setSubject("수정된 제목");
-        this.questionRepository.save(q);
-
+        questionRepository.save(q);
     }
 
+    /*
+    SQL
+    DELETE
+    FROM
+        question
+    WHERE
+        id = ?
+    */
     @Test
     @DisplayName("데이터 삭제하기")
-    void t008(){
-        assertEquals(2, this.questionRepository.count());
-        Optional<Question> oq = this.questionRepository.findById(1);
+    void t008() {
+        // questionRepository.count()
+        // SQL : SELECT COUNT(*) FROM question;
+        assertEquals(2, questionRepository.count());
+        Optional<Question> oq = questionRepository.findById(1);
         assertTrue(oq.isPresent());
         Question q = oq.get();
         questionRepository.delete(q);
-        assertEquals(1, this.questionRepository.count());
+        assertEquals(1, questionRepository.count());
     }
-
 }
