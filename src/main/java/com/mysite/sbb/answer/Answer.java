f947292c4bc5777a9ac900603a2c462b56191345
chkim429 <ckdgjs000@naver.com>
2023-03-22 11:18:52 +0900

    ãSQL LOG를 더 자세하게

--- src/main/java/com/mysite/sbb/answer/Answer.java
+++ src/main/java/com/mysite/sbb/answer/Answer.java
@@ -4,12 +4,14 @@ import com.mysite.sbb.question.Question;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
+@ToString
 public class Answer {
     @Id // PRIMARY KEY
     @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
@@ -21,6 +23,7 @@ public class Answer {
     private LocalDateTime createDate; // DATETIME
 
     @ManyToOne
+    @ToString.Exclude
     private Question question;
     // `public class Answer` @ManyToOne `private Question question`
 }
